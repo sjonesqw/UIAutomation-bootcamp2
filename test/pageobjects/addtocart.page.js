4654f074d81b66f21749ae4aaffcfd56c5a6fc4b
sjonesqw <sjones@qualityworkscg.com>
2023-07-25 18:28:12 -0400

    Completed add to cart test

--- test/pageobjects/addtocart.page.js
+++ test/pageobjects/addtocart.page.js
@@ -1,83 +1,144 @@
 
 
+//const { $ } = require('webdriverio/build/commands/element');
+//require { Key } from 'webdriverio';
+
 const Page = require('./page');
 
 class addtocart extends Page{
 
-    get selectitem () {
+    get firstItemBtn () { 
         return $('//div[@id="product-0"]//button[@id="add-to-cart"]');
+        
     }
-    get mitem1 () {
+    get firstItem () { 
         return $('//div[@id="product-1"]//button[@id="add-to-cart"]');
     }
-    get mitem2 () {
-        return $('//div[@id="product-2"]//button[@id="add-to-cart"]');
+    get firstItemImg (){ 
+        return $('//img[@src="/images/quality-hat-model.jpg"]');
     }
-    get singleitemprdctpage(){
-        return $('//div[@id="product-0"]//img[@class="chakra-image css-2i84d9"]');
+    
+    get thirdItemImg(){
+        return $('#product-2 > div.css-5ge9zd > div > img');
     }
-    get mitem1prdctpage (){
-        return $('//div[@id="product-1"]//img[@class="chakra-image css-2i84d9"]')
+    get fourthItem(){
+        return $('#product-3 > div.css-5ge9zd > div > img');
     }
-    get mitem2prdctpage (){
-        return $('//div[@id="product-2"]//img[@class="chakra-image css-2i84d9"]')
-    }
-    get addtocartprdctpage (){
+    
+    get addToCartProductPage (){
         return $('//button[@id="add-to-cart"]')
     }
 
-    get signoutbtn(){
-        return $('//button[@id="top-sign-out"]');
+    get cartItemsList (){ 
+        return $('#snipcart > div > div > div.snipcart-layout__content.snipcart-layout__content--side.snipcart-cart--edit > section > ul');
     }
-    get shoppingcart(){
-        return $('//button[@id="top-cart"]');
-    }
-
-    get detailedcart (){
-        return $('//button[normalize-space()="View detailed cart"]');
-    }
-    get cartsummary (){
-        return $('//section[@class="snipcart-cart__content"]');
-    }
-
-    get rmvitem () {
+    
+    get rmvItem () {
 
         return $('button[title="Remove item"]');
     }
+
+    get secondItem (){
+        return $('div[id="product-1"] input[value="1"]');
+    }
+    get secondItemBtn (){
+        return $('//div[@id="product-1"]//button[@id="add-to-cart"]');
+    }
+    
+    get secondItemUpArrow (){
+        return $('#product-1-increase');
+    }
+    get secondItemDownArrow (){
+        return $('#product-1-decrease');
+    }
+    get detailPageIncrease(){
+        return $('#product-increase');
+    }
+    get detailPageDecrease (){
+        return $('#product-decrease');
+    }
+    get itemCountInCart1 (){
+        return $('.snipcart-item-quantity__quantity.snipcart__font--std');
+    }
+    get backToProducts (){
+        return $('.chakra-heading.css-18j379d');
+    }
+    get qualityPillow (){
+       return $('#product-4 > div.chakra-stack.css-n21gh5 > div > div.chakra-numberinput.css-9renge > input');
+    }
+    get qualityPillowBtn (){
+        return $('//div[@id="product-4"]//button[@id="add-to-cart"]');
+    }
+    get qualityMousePad (){
+        return $('#__next > div.chakra-container.css-1326l4r > div > div.css-1p34w40 > div.chakra-stack.css-egoftb > div.chakra-numberinput.css-9renge > input');
+    }
     
 
-    //add multiple of a single item to cart, from gallery page
-    async addsingleitem () {
-        await this.selectitem.waitForDisplayed(1500);
-        await this.selectitem.click();
+
+    //add the first item from gallery page to the cart 
+    
+    async addSingleItem1 () {
+        await this.firstItem.waitForDisplayed(1500);
+        await this.firstItemImg.scrollIntoView();
+        await this.firstItemBtn.click();
         await browser.pause(2000);
     }
-
-    //add more than one types of items to the cart, from gallery page
-    async addmultiplteitems () {
-        await this.mitem1.waitForDisplayed(2000);
-        await this.mitem1.click();
-        await browser.pause(1500);
-        await this.mitem2.click();
+    
+    async specifyCountGalleryPage (count) {
+        await this.qualityPillow.isDisplayed();
+        await this.qualityPillow.click();
+        await this.qualityPillow.clearValue();
+        await browser.pause(2000);
+        await this.qualityPillow.clearValue();
+        await this.qualityPillow.setValue(count);
+        await this.qualityPillowBtn.click();
+        await browser.pause(5000);
+        
+       
     }
 
-    //add multiple of a single item to cart, from product detail page
-    async addsingleprdctpage () {
-        await this.singleitemprdctpage.click();
-        await this.addtocartprdctpage.click();
+    async specifyCountDetailPage (count) {
+        await this.thirdItemImg.isDisplayed();
+        await this.thirdItemImg.click();
+        await this.qualityMousePad.isDisplayed();
+        await this.qualityMousePad.click();
+        await this.qualityMousePad.clearValue();
+        await this.qualityMousePad.setValue('6');
+        await this.addToCartProductPage.click();
     }
 
 
 
-    //signout after each to get a fresh cart
-    async signout(){
-        await this.signoutbtn.click();
+    //increase the quantity of the item to be added to the cart using the arrows - product gallery
+    async increaseSecondItemCount (count){
+        for (let x=0;x<count;x++){
+            await this.secondItemUpArrow.click();
+        }
     }
 
-    // called multiple times to remove items from cart
-    async rmvitems(){
-        await this.rmvitem.click();
+    //decrease the quantity of the item to be added to the cart using the arrows - product gaellery
+    async decreaseSecondItemCount (count){
+        for (let x=0;x<count;x++){
+            await this.secondItemDownArrow.click();
+        }
     }
+
+    //increase the quantity of the item to be added to the cart using the arrows - product detail page
+    async increaseCountProductDetail (count){
+        await this.detailPageIncrease.waitForDisplayed(1500);
+        for (let x=0;x<count;x++){
+            await this.detailPageIncrease.click();
+        }
+    }    
+
+    //decrease the quantity of the item to be added to the cart using the arrows - product detail page
+    async decreaseCountProductDetail (count){
+        await this.detailPageDecrease.waitForDisplayed(1500);
+        for (let x=0;x<count;x++){
+            await this.detailPageDecrease.click();
+        }
+    } 
+
 
     open () {
         return super.open('');
--- test/specs/addtocart.e2e.js
+++ test/specs/addtocart.e2e.js
@@ -3,42 +3,86 @@ const cartpage = require('../pageobjects/addtocart.page');
 
 
 
-describe('My add to cart test ', () => {
+describe('My add to cart tests. Verify that a user is able to add items to cart from the gallery page and from the product details page. Verify that a user is able to specify the number of items to add by using the arrows or entering a number in the text field', () => {
 
-    it('should login to the application and add the first item on the page to the users cart', async () => {
+    it('should login to the application and add the first item on the page to the cart', async () => {
         await LoginPage.open();
         await LoginPage.login("test@mailinator.com", "Stephanie1!");
         await browser.pause(2000);
-        await browser.url('https://ui-automation-camp.vercel.app/products');
-        await cartpage.addsingleitem();
-        expect(cartpage.cartsummary.toHaveText("Quality Fitted Hat"));
-        await cartpage.rmvitem.click();
+        await expect(browser).toHaveUrl('https://ui-automation-camp.vercel.app/products');
+        await cartpage.addSingleItem1();
+        await expect(cartpage.cartItemsList).toHaveTextContaining("Quality Fitted Hat");
+        await cartpage.rmvItem.click();
+        await browser.pause(2000);
+        
+    });
+
+    it('should use the arrows to increase & decrease the count of the product before adding it to the cart', async () => {
+        
+        await cartpage.increaseSecondItemCount(4);
+        await cartpage.decreaseSecondItemCount(2);
+        await cartpage.secondItemBtn.click();
+        await cartpage.itemCountInCart1.waitForDisplayed();
+        await expect(cartpage.itemCountInCart1).toHaveTextContaining("3");
+        await expect(cartpage.cartItemsList).toHaveTextContaining("Quality Trucker Hat");
+        await cartpage.rmvItem.click();
+        await browser.pause(2000);
+
+    });
+    it('should enter a number in the text field then add the item  to the cart', async () => {
+
+        await cartpage.specifyCountGalleryPage(4);
+        await expect(cartpage.itemCountInCart1).toHaveTextContaining("14");
+        await expect(cartpage.cartItemsList).toHaveTextContaining("Quality Pillow");
+        await cartpage.rmvItem.click();
+        await browser.pause(2000);
        
         
     });
-    it('should add the second and third items on the page to the users cart', async () => {
-        
-    
 
-        await browser.url('https://ui-automation-camp.vercel.app/products');
-        await cartpage.addmultiplteitems();
-        expect(cartpage.cartsummary.toHaveText("Quality Trucker Hat"));
-        expect(cartpage.cartsummary.toHaveText("Quality Mousepad"));
+    it('should navigate to the product detail pages of the third item in the gallery and add it to the cart', async () => {
+        
+        await cartpage.thirdItemImg.waitForDisplayed(1500);
+        await cartpage.thirdItemImg.click();
         await browser.pause(2000);
-        await cartpage.rmvitem.click();
-        await cartpage.rmvitem.click();
+        await expect(browser).toHaveUrl('https://ui-automation-camp.vercel.app/products/quality-mousepad');
+        await cartpage.addToCartProductPage.waitForDisplayed(1500);
+        await cartpage.addToCartProductPage.click();
         await browser.pause(1500);
-        await browser.url('https://ui-automation-camp.vercel.app/products');
+        await expect(cartpage.cartItemsList).toHaveTextContaining("Quality Mousepad");
+        await cartpage.rmvItem.click();
+        await cartpage.backToProducts.click();
         
     });
-    it('should login to the application, navigate to the product detail pages of the first item in the gallery and add it to the cart', async () => {
+
+    it('should navigate to the product detail pages of the fourth item in the gallery use the arrows to increase & decrease the count of the product before adding it to the cart', async () => {
         
-        
-        await cartpage.addsingleprdctpage();
+        await cartpage.fourthItem.waitForDisplayed(1500);
+        await cartpage.fourthItem.click();
+        await browser.pause(2000);
+        await expect(browser).toHaveUrl('https://ui-automation-camp.vercel.app/products/quality-mug'); 
+        await cartpage.increaseCountProductDetail(5);
+        await cartpage.decreaseCountProductDetail(2);
+        await cartpage.addToCartProductPage.click();
+        await cartpage.cartItemsList.waitForDisplayed(1500);
+        await expect(cartpage.cartItemsList).toHaveTextContaining("Quality Mug");
+        await expect(cartpage.itemCountInCart1).toHaveTextContaining("4");
+        await cartpage.rmvItem.click();
+        await cartpage.backToProducts.click();
         await browser.pause(1500);
-        expect(browser.url('https://ui-automation-camp.vercel.app/products/quality-hat-model'));
-        expect(cartpage.cartsummary.toHaveText("Quality Fitted Hat"));
-        //await browser.url('https://ui-automation-camp.vercel.app/products');
+        
+        
+    });
+    
+
+    it('should enter a number in the text field on the product detail page then add the item to the cart', async () => {
+        
+        await cartpage.specifyCountDetailPage(6);
+        await expect(browser).toHaveUrl('https://ui-automation-camp.vercel.app/products/quality-mousepad');
+        await expect(cartpage.cartItemsList).toHaveTextContaining("Quality Mousepad","16");
+        await expect(cartpage.itemCountInCart1).toHaveTextContaining("16");
+        
+       
         
     });
 
